e344f9842bb1bfb7436356a679f1f2e25484cea0
RasulRustamli <rasular@code.edu.az>
2022-09-19 03:25:12 +0400

    Add project files.

---
+++ Gelf_Log_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gelf_Log_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
